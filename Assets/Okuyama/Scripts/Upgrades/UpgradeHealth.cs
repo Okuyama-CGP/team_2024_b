032cb8725d14b97aeab89f5582380e67f1507ee4
Okuyama <60276309.okuyama.cgp@gmail.com>
2024-09-14 16:49:40 +0900

    アプグレいくつか追加
    アップグレードをいくつか追加した。

--- Assets/Okuyama/Scripts/Upgrades/UpgradeHealth.cs
+++ Assets/Okuyama/Scripts/Upgrades/UpgradeHealth.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// MaxHPを増やすアップグレード
+/// </summary>
 [CreateAssetMenu(fileName = "UpgradeHealth", menuName = "Upgrades/UpgradeHealth")]
-public class UpgradeHealth : BaseUpgrade
-{
-    const float healthBonus = 10f;
-    const float healthBonusPerStack = 5f;
+public class UpgradeHealth : BaseUpgrade {
 
-    public override void OnAdded(PlayerCore player)
-    {
+    [Space]
+    [SerializeField] float healthBonus = 10f;
+    [SerializeField] float healthBonusPerStack = 5f;
+
+    public override void OnAdded(PlayerCore player) {
         player.IncreaseMaxHP(healthBonus);
     }
 
-    public override void OnStacked(PlayerCore player)
-    {
+    public override void OnStacked(PlayerCore player) {
         player.IncreaseMaxHP(healthBonusPerStack);
     }
 }
--- Assets/Okuyama/Scripts/Upgrades/UpgradeOnhitTest.cs
+++ Assets/Okuyama/Scripts/Upgrades/UpgradeOnhitTest.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "UpgradeOnhitTest", menuName = "Upgrades/UpgradeOnhitTest")]
-public class UpgradeOnhitTest : BaseUpgrade
-{
+public class UpgradeOnhitTest : BaseUpgrade {
+    
     [Space]
     [SerializeField] GameObject particle;
 
-    public override void OnAdded(PlayerCore player)
-    {
+    public override void OnAdded(PlayerCore player) {
         player.OnHitEvent += OnHitEffect;
         Debug.Log("OnHit追加");
     }
 
-    public override void OnStacked(PlayerCore player)
-    {
+    public override void OnStacked(PlayerCore player) {
         Debug.Log("OnHitスタック : " + stackCount);
     }
 
     //OnHit処理の内容
-    public void OnHitEffect(GameObject target, Damage damage)
-    {
+    public void OnHitEffect(GameObject target, Damage damage) {
         //パーティクル生成
         Vector3 pos = target.transform.position + new Vector3(0, 1, 0);
         Instantiate(particle, pos, Quaternion.identity);
