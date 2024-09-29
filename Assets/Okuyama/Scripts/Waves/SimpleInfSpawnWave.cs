da87090e7e2ac4e24bb795f215db66e7a9462e39
Okuyama <60276309.okuyama.cgp@gmail.com>
2024-09-30 01:55:51 +0900

    敵HPバー実装

--- Assets/Okuyama/Scripts/Waves/SimpleInfSpawnWave.cs
+++ Assets/Okuyama/Scripts/Waves/SimpleInfSpawnWave.cs
@@ -2,39 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleInfSpawnWave : BaseWave
-{
+public class SimpleInfSpawnWave : BaseWave {
     [SerializeField] float spawnInterval = 1.0f;
     [SerializeField] GameObject enemyPrefab;
 
     float elapsedTime = 0;
     PlayerCore playerCore;
 
-    public override void OnStartWave()
-    {
+    public override void OnStartWave() {
         Debug.Log(name + " Start");
         playerCore = MainGameManager.instance.playerCore;
     }
 
-    public override void OnWaveUpdate()
-    {
+    public override void OnWaveUpdate() {
         elapsedTime += Time.deltaTime;
         if (elapsedTime >= spawnInterval) //Intervalごとに敵を生成
         {
             //距離20の円周上にランダムに敵を生成
-            //FIXME マップ外スポーン対策
             float dist = 20;
-            float deg = UnityEngine.Random.Range(0, Mathf.PI * 2);
+            Vector3 spawnPos;
 
-            Vector3 spawnPos = new Vector3(Mathf.Cos(deg), 0, Mathf.Sin(deg)) * dist;   //極座標変換
-            spawnPos += playerCore.position;
+            while (true) {
+                //ゲームエリア外なら振りなおす
+                float deg = UnityEngine.Random.Range(0, Mathf.PI * 2);
+
+                spawnPos = new Vector3(Mathf.Cos(deg), 0, Mathf.Sin(deg)) * dist;   //極座標変換
+                spawnPos += playerCore.position;
+
+                if (MainGameManager.instance.IsInGameArea(spawnPos)) {
+                    break;
+                }
+            }
+
             GameObject enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
             elapsedTime -= spawnInterval;
         }
     }
 
-    public override void OnEndWave()
-    {
+    public override void OnEndWave() {
         Debug.Log(name + "End");
     }
 }
